36b34935c824eeec104facf9d70aff0ff67db835
PRubens2001 <rubensfpateis.hs@gmail.com>
2026-04-14 17:18:32 -0300

    Delete GYM_TRACK directory

--- GYM_TRACK/src/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://wvkqpekqivpxrndawqhz.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_B2Nf2b8JU4anBgfNjJHrkA_bgLlGEf-';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
